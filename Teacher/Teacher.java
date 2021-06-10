e4bad743949b6304c845b8d506463befb3798eba
Cse222-Group12-Project <85703463+Cse222-Group12-Project@users.noreply.github.com>
2021-06-10 23:43:27 +0300

    Add files via upload

---
+++ Teacher/Teacher.java
@@ -0,0 +1,13 @@
+public class Teacher extends Person implements Comparable<Teacher>{
+    public Teacher(String name, String surname) {
+        super(name, surname);
+    }
+    @Override
+    public int compareTo(Teacher o) {
+        if (this.getName().compareTo(o.getName())<0)
+                return -1;
+        else if(this.getName().compareTo(o.getName())>0)
+            return 1;
+        return 0;
+    }
+}
