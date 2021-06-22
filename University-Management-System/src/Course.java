0bb37d71bc9941fa6c611ef7b4d59d7a32a2637c
Cse222-Group12-Project <85703463+Cse222-Group12-Project@users.noreply.github.com>
2021-06-22 16:57:20 +0300

    Add files via upload
    Course class create

---
+++ University-Management-System/src/Course.java
@@ -0,0 +1,67 @@
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+public class Course {
+    private int capacity;
+    private String coursename;
+    private Teacher courseteacher;
+    private LinkedList<Student> students;
+    private PriorityQueue<Homework> homework;
+    private Stack<String> message;
+
+    public Course(int capacity, String coursename) {
+        this.capacity = capacity;
+        this.coursename = coursename;
+        this.courseteacher = courseteacher;
+        this.students=new LinkedList<Student>();
+        this.homework=new PriorityQueue<Homework>();
+        this.message=new Stack<String>();
+    }
+
+    public PriorityQueue<Homework> getHomework() {
+        return homework;
+    }
+
+    public void setHomework(PriorityQueue<Homework> homework) {
+        this.homework = homework;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getCoursename() {
+        return coursename;
+    }
+
+    public void setCoursename(String coursename) {
+        this.coursename = coursename;
+    }
+
+    public Teacher getCourseteacher() {
+        return courseteacher;
+    }
+
+    public void setCourseteacher(Teacher courseteacher) {
+        this.courseteacher = courseteacher;
+    }
+
+    public LinkedList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(LinkedList<Student> students) {
+        this.students = students;
+    }
+    public void addHomework(Homework hw){
+        this.homework.add(hw);
+    }
+    public void addMessage(String messsage){
+        this.message.push(messsage);
+    }
+}
