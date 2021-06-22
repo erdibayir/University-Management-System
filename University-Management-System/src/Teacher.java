2b80d960169c6a73cf22f46670aebaa6693eda3f
Cse222-Group12-Project <85703463+Cse222-Group12-Project@users.noreply.github.com>
2021-06-22 15:13:09 +0300

    Update Teacher.java
    Teacher class update

--- University-Management-System/src/Teacher.java
+++ University-Management-System/src/Teacher.java
@@ -10,6 +10,17 @@ public class Teacher extends Person implements Comparable<Teacher>{
             return 1;
         return 0;
     }
+    public void addHomework(Homework hw,Course cs,Database db){
+
+    }
+    public void addMessageCourse(String ms,Course cs,Database db){
+
+    }
+    public void enterGrade(int not,Student student,Course cs,Database db){
+
+    }
+    
+    
     public Student searchStudent(Database database){
         database.getStudents();
         return null;
