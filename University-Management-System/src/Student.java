52842a91a61382a43f21eb81c2b2cf0415dbfc88
Cse222-Group12-Project <85703463+Cse222-Group12-Project@users.noreply.github.com>
2021-06-22 16:56:01 +0300

    Update Student.java
    Student update

--- University-Management-System/src/Student.java
+++ University-Management-System/src/Student.java
@@ -1,9 +1,34 @@
-public class Student implements Comparable<Student> {
-    /*
-    NEED IMPLEMENTATION
-     */
+import java.util.Hashtable;
+public class Student extends Person implements Comparable<Student>{
+    private String StudentNo;
+    private Hashtable<Course,Integer> lessons; ///Notu ve notun hangi course ait olduğu
+    public Student(String name,String surname,String password) {
+        super(name,surname,password);
+        lessons= new Hashtable<>();
+    }
+
+    public String getStudentNo() {
+        return StudentNo;
+    }
+
+    public void setStudentNo(String studentNo) {
+        StudentNo = studentNo;
+    }
+
     @Override
     public int compareTo(Student o) {
         return 0;
     }
+    public void addNot(int not,Course course){
+        lessons.put(course,not);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "StudentName='" + getName() + '\'' +
+                ", Surname=" +this.getSurname()  +
+                '}';
+    }
 }
+
