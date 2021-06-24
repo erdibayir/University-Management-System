deccb16bccc0997f96ed01b585cad98e56e453dc
Cse222-Group12-Project <85703463+Cse222-Group12-Project@users.noreply.github.com>
2021-06-24 09:31:59 +0300

    Update Main.java
    V1

--- University-Management-System/src/Main.java
+++ University-Management-System/src/Main.java
@@ -26,12 +26,67 @@ public class Main {
                 int choose = scanner.nextInt();
                 if (choose == 1) {
                     System.out.println("***AddHomework***");
+                    System.out.println("Enter The Description Of The Homework");
+                    scanner.nextLine();
+                    String description=scanner.nextLine();
+                    System.out.println("Enter The Date (Assignments will only be entered as the day of the requested date.)");
+                    String date=scanner.nextLine();
+
+                    for (int i = 0; i <db.getCourse().size(); i++) {
+                        System.out.println(i+"-"+db.getCourse().get(i).getCoursename());
+                    }
+                    System.out.println("Please choose which course you want to add this assignment to.");
+                    int sec=scanner.nextInt();
+                    if (sec<=db.getCourse().size()){
+                        Course cs=db.getCourse().get(sec);
+                        Homework hw=new Homework(description,date);
+                        teacher.addHomework(hw,cs,db);
+                    }
+                    else
+                        System.out.println("The given number is not in the desired range");
 
                 } else if (choose == 2) {
                     System.out.println("***AddMessageCourse***");
+                    System.out.println("Enter The Message");
+                    String ms=scanner.nextLine();
+                    for (int i = 0; i <db.getCourse().size(); i++) {
+                        System.out.println(i+"-"+db.getCourse().get(i).getCoursename());
+                    }
+                    System.out.println("Please choose which course you want to add this Message");
+                    int sec=scanner.nextInt();
+                    if (sec<=db.getCourse().size()){
+                        Course cs=db.getCourse().get(sec);
+                        teacher.addMessageCourse(ms,cs,db);
+                    }
+                    else
+                        System.out.println("The given number is not in the desired range");
 
                 } else if (choose == 3) {
                     System.out.println("***EnterGrade***");
+                    System.out.println(db.getStudents().inorder());
+                    System.out.println("Enter Student Name:");
+                    scanner.nextLine();
+                    String studentname=scanner.nextLine();
+                    System.out.println("Enter Student SurName:");
+                    String studentsurname=scanner.nextLine();
+                    System.out.println("Enter Student No:");
+                    String studentno=scanner.nextLine();
+                    Student st=new Student(studentname,studentsurname,studentno);
+                    if(db.getStudents().find(st)!=null){
+                        st.getstudentCourse();
+                        System.out.println("Type the name of the course you want to enter the grade for");
+                        String coursename=scanner.nextLine();
+                        System.out.println("Enter The Grade");
+                        int grade=scanner.nextInt();
+                        Course course=new Course(coursename);
+                        st.addNot(grade,course);
+                        st.getstudentCourse();
+                    }
+                    else {
+                        System.out.println("Not Found This Student");
+                    }
+
+
                 }
                 else if (choose==4){
                     System.out.println("Exiting...");
